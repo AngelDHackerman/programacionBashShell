97d3d316b83cfc868ae0467e5aff43bfc9f626e1
angelDariaux <angeldariaux@gmail.com>
2022-09-11 13:14:20 -0600

    usando argumentos

---
+++ argumentos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Programa para revisar los tipos de operadores
+
+nombreCurso=$1
+horarioCurso=$2
+
+echo "El nombre del curso es: $nombreCurso y el horario es: $horarioCurso"
+echo "El numero de parametros enviados es: $#"
+echo "Los parametros enviados son: $*"
