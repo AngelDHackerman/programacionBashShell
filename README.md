a56aebf4be7bdb2cab3af9d957e845d8de24b25a
angelDariaux <angeldariaux@gmail.com>
2022-09-11 18:23:51 -0600

    modificado el readme con info importante

--- README.md
+++ README.md
@@ -6,14 +6,28 @@ In this repo you will find some usefull tips about how the bash scripting works.
 
 `bash -x fileName.sh`Used to display the information of the commands that are used, capturing the command and its output.
 
+#### Vimrc options:
+
+`:colorscheme desert` this change the color them of vim
+`set showmode`  this will show the operation that we are doing
+`set autoindent`
+`set tabstop=2 `  spaces with each tap hit
+`set expandtab`  ajust the spaces based on the hierarchy
+`syntax on`  it will give different colors based on the syntax
+
+see more here: https://vimconfig.com/ 
+
+(remove the comments before use in the real .vimrc)
+
+
 #### Read options:
 
 
-`read -s` -> Hide incoming information by user itself.
+`read -s` Hide incoming information by user itself.
 
-`read -n1` -> Keeps the execution line, the same line we are in until it reaches the defined number of characters it can be 1 or 2 or 3 ... etc.
+`read -n1` Keeps the execution line, the same line we are in until it reaches the defined number of characters it can be 1 or 2 or 3 ... etc.
 
-`read -p` -> Indicates that the information entered must be left in the following variable.
+`read -p` Indicates that the information entered must be left in the following variable.
 
 #### Echo options: 
 
--- arrays.sh
+++ arrays.sh
@@ -5,7 +5,9 @@ arregloCadenas=(Marco, Antonio, Pedro, Susana)
 arregloRangos=({A..Z} {10..20})  # ? ({A..Z} {10..20}) this is how the ranges are made.
 
 
-# Imprimir todos los valores:
+
+echo -e "\n\t Imprimir todos los valores:"
+      # todo: Imprimir todos los valores
 
 echo "Arreglo de Numeros: ${arregloNumeros[*]}"
 echo "Arreglo de Cadenas: ${arregloCadenas[*]}"
@@ -13,8 +15,31 @@ echo "Arreglo rangos: ${arregloRangos[*]}"  # ! * or @, are the same, both show 
 # echo "Arreglo rangos: ${arregloRangos[@]}"
 
 
-echo -e "\n"
-# Imprimir los tamaños de los arreglos:
+
+echo -e "\n\t Imprimir los tamaños de los arreglos:"
+      # todo: Imprimir los tamaños de los arreglos:
 
 echo "Tamaño array numeros: ${#arregloNumeros[*]}"  # ! #arrayName, prints the length of the array
+echo "Tamaño array cadenas: ${#arregloCadenas[*]}"  
+echo "Tamaño array rangos: ${#arregloRangos[*]}"  
 
+
+
+echo -e "\n\t Imprimir la posicion 3 de cada arreglo:"
+      # todo: Imprimir la posicion 3 de cada arreglo:
+
+echo "Posicion 3 Arreglo numeros: ${arregloNumeros[3]}"  # ? the count starts from 0.
+echo "Posicion 3 Arreglo Cadenas: ${arregloCadenas[3]}"
+echo "Posicion 3 Arreglo Rangos: ${arregloRangos[3]}"
+
+
+
+echo -e "\n\t Añadir y eliminar valores en un arreglo:"
+      # todo: Añadir y eliminar valores en un arreglo:
+
+arregloNumeros[7]=20
+unset arregloNumeros[0]  # ! this removes the element but also removes the index in the array
+echo "Arreglo de numeros: ${arregloNumeros[*]}"
+echo "Tamaño arreglo de numeros: ${#arregloNumeros[*]}"
+
+echo "elemnt 0: ${arregloNumeros[6]}"
